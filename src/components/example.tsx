09dbdd63e716f6bac80dc0bacb998ecd9f821e8c
ryu <114303361+ryuapp@users.noreply.github.com>
2025-01-02 17:02:15 +0900

    Add ag-grid

---
+++ src/components/example.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import {useState } from "react";
+import { AllCommunityModule, ModuleRegistry, ColDef } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+// Row Data Interface
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
+
+// Create new GridExample component
+export const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState<IRow[]>([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+
+  // Container: Defines the grid's theme & dimensions.
+  return (
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+   
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { GridExample } from '../components/example'
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
@@ -7,8 +8,11 @@ export const Route = createFileRoute('/')({
 
 function HomeComponent() {
   return (
-    <div className="p-2">
+    <div className="p-2 flex flex-col">
       <h3>Welcome Home!</h3>
+      <div className='w-full h-[400px]'>
+      <GridExample />
+      </div>
     </div>
   )
 }
