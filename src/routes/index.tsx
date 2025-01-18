1736dc43dc231ea31ec5665c43f67f4f686743bc
ryu <114303361+ryuapp@users.noreply.github.com>
2025-01-18 11:52:01 +0900

    Add title and description

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,20 @@ export const Route = createFileRoute("/")({
 
 function HomeComponent() {
   return (
-    <div className="flex flex-col p-2">
+    <div className="flex flex-col gap-4 p-2">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-bold text-5xl">Table Master</h1>
+        <p>
+          This website is an example of csv file operation using{" "}
+          <a
+            className="text-blue-500 underline"
+            href="https://www.ag-grid.com/"
+          >
+            AG Grid
+          </a>
+          .
+        </p>
+      </div>
       <div>
         <Table />
       </div>
